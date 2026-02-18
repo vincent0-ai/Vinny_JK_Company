18afa4b80998012bbc75d08d0748f74660210100
Vinny <petertom13249@gmail.com>
2026-02-18 11:59:43 +0300

    Initialize Django project settings with environment variable integration and add CSRF trusted origins to example.

--- vinny_kj/settings.py
+++ vinny_kj/settings.py
@@ -31,6 +31,8 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-fallback-key-for-dev-only'
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
+CSRF_TRUSTED_ORIGINS = os.getenv('CSRF_TRUSTED_ORIGINS', 'https://*.caprover.com,https://*.127.0.0.1').split(',')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 
 # Application definition
