2768949aa75c970dc60d94b83329d2dffc91bcc2
Vinny <vincentochi041@gmail.com>
2026-02-24 22:24:32 +0300

    config: Move SQLite database file to 'data' subdirectory.

--- vinny_kj/settings.py
+++ vinny_kj/settings.py
@@ -97,7 +97,7 @@ WSGI_APPLICATION = 'vinny_kj.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'data' / 'db.sqlite3',
     }
 }
 
